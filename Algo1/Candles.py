ab3165ff9fa29ecaaa5ad1c652005b95a24907d6
sdcreek240 <aidandwsn@gmail.com>
2024-11-26 13:19:58 +0200

    debugApp_Started

---
+++ Algo1/Candles.py
@@ -0,0 +1,47 @@
+
+import MetaTrader5 as mt5
+import pandas as pd
+from datetime import datetime
+
+from tabulate import tabulate
+
+#Define colors for output formatting
+COLOR_OPEN = '\033[92m'#Green
+COLOR_CLOSE = '\033[94m'#Blue
+COLOR_HIGH = '\033[93m'#Yellow
+COLOR_LOW = '\033[91m'#Red
+COLOR_RESET = '\033[0m'#Reset color 
+
+class Candles:
+
+    def __init__(self, symbol, timeframe, n):
+        
+        #Initialise member variables
+        self.symbol = symbol
+        self.timeframe = timeframe
+        self.n = n
+        self.candles = []
+
+        #Call fetchCandles() to populate candles
+        self.fetchCandles()
+
+    #Fetch Candle data and populate candles member variable with it
+    def fetchCandles(self):
+
+        #Fetch n candles data
+        rates = mt5.copy_rates_from_pos(self.symbol, self.timeframe, 0, self.n)
+
+        #Check if candles were fetched
+        if rates is None:
+            print(f"Failed to fetch candles for {self.symbol}. Error: ", mt5.last_error())
+            return
+        
+        #Create Pandas DataFrame - Easier Processing
+        self.candles = pd.DataFrame(rates)
+        self.candles['time'] = pd.to_datetime(self.candles['time'], unit='s')#Specifies that data already in frame time column is in seconds and should be converted to date time
+
+    #Define string representation of Candles object
+    def __repr__(self):
+        return f"Candles[{self.symbol}]\n{self.candles[['time', 'open', 'high', 'low', 'close']]}"
+
+        
--- Algo1/debugApp.py
+++ Algo1/debugApp.py
@@ -7,39 +7,47 @@ import pandas as pd
 from metaInterface import timeframe_map
 
 
-class debugApp:
+class debugApp:    
 
     def LeftFrame(self):
         
-        frmLeft = Frame(root, bg="#32414e", bd=10, cursor="dot", width=200, height=1600)
+        frmLeft = Frame(root, bg="#32414e", bd=10, cursor="dot", width=225, height=1600)
         frmLeft.grid(row=0, column=0, padx=5, sticky="ns")
         frmLeft.grid_propagate(False)
 
         #Make column 0 expandable
         frmLeft.grid_columnconfigure(0, weight=1)
 
-        frmSample = Frame(frmLeft, bg="#74d1c9", bd=5, height=50, width=800)
+        frmSample = Frame(frmLeft, bg="#74d1c9", bd=5, height=50, width=225)
         frmSample.grid(row=0, column=0)
 
         #Define list for symbol
         self.symbols = ["EURUSD", "GBPUSD", "USDJPY", "AUDUSD"]
 
-        cmbSymbol = ttk.Combobox(frmSample, values=self.symbols, width=8, cursor="dot")
-        cmbSymbol.set(self.symbols[0])
-        cmbSymbol.grid(row=0, column=0, padx=5)
+        self.cmbSymbol = ttk.Combobox(frmSample, values=self.symbols, width=8, cursor="dot")
+        self.cmbSymbol.set(self.symbols[0])
+        self.cmbSymbol.grid(row=0, column=0, padx=5)
 
         #Define timeframe map
         self.timeframe_map = timeframe_map
 
-        cmbTimeframe = ttk.Combobox(frmSample, values=list(self.timeframe_map.keys()), width=5, cursor="dot")
-        cmbTimeframe.grid(row=0, column=2, padx=15)
-        cmbTimeframe.set("5M")
+        self.cmbTimeframe = ttk.Combobox(frmSample, values=list(self.timeframe_map.keys()), width=5, cursor="dot")
+        self.cmbTimeframe.grid(row=0, column=2, padx=5)
+        self.cmbTimeframe.set("5M")
 
         #Create Button to fetch live Data
-        btnFetchLive = Button(frmSample, text="Live", width=5, hieght=2.5, command=FecthLive())
+        self.btnFetchLive = Button(frmSample, text="Live", width=4, height=2, 
+                              command=self.FetchLive)
+        self.btnFetchLive.grid(row=1, column=1)
 
         #Figure out how to retrieve values from comboboxes and write function to plot chart of data fetched
 
+    def FetchLive(self):
+
+        symbol = self.cmbSymbol.get()
+        timeframe = self.timeframe_map.get(self.cmbTimeframe.get())
+
+        print(timeframe)
 
     def __init__(self, root):
         print("---Entry_debugApp---")
